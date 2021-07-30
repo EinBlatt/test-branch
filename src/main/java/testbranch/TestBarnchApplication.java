f61ca7873fcc564c80c5e4c62955c57c363db092
einblatt <einblatt@163.com>
2021-07-30 09:03:08 +0800

    commit init

---
+++ src/main/java/testbranch/TestBarnchApplication.java
@@ -0,0 +1,13 @@
+package testbranch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBarnchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBarnchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/testbranch/TestBarnchApplicationTests.java
@@ -0,0 +1,13 @@
+package testbranch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBarnchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
